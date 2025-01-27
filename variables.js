80abfd02727a078481b8956e611696dc51243c05
MandemGibson <themaingib@gmail.com>
2025-01-27 23:38:35 +0000

    taught students variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+let name = "John";
+let age = 23;
+let accountBalance = 23000.56;
+
+console.log(
+  name +
+    " who is " +
+    age +
+    " years old has " +
+    accountBalance +
+    " in his bank account."
+);
