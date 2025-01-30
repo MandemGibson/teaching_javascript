1fac839a6579e2527725e1cc8bb4421e24a74ce3
MandemGibson <themaingib@gmail.com>
2025-01-30 23:31:21 +0000

    while loop

--- loops.js
+++ loops.js
@@ -1,20 +1,25 @@
-const arr = [1,2,3,4]
+const arr = [1, 2, 3, 4];
 
 //print indices
-for(const num in arr){
-    console.log(num);  
+for (const num in arr) {
+  console.log(num);
 }
 
 //print values
-for(const num of arr){
-    console.log(num);
-    
+for (const num of arr) {
+  console.log(num);
 }
 
 //print values
-for(let i = 0; i < arr.length; i++){
-  console.log("number ",arr[i])
+for (let i = 0; i < arr.length; i++) {
+  console.log("number ", arr[i]);
 }
 
 //print values
-arr.forEach((num)=> console.log(num))
+arr.forEach((num) => console.log(num));
+
+let i = 0;
+while (i < arr.length) {
+  console.log("This is loop", i);
+  i++;
+}
