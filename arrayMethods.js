74d4c49c39ce7ae8536acf5600d53415daa7a979
MandemGibson <themaingib@gmail.com>
2025-02-02 23:47:34 +0000

    add reduce method to arrayMethods.js for summing numbers

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,9 +1,14 @@
-let nameArr = ["John", "Mane","James", "Benard"]
+let nameArr = ["John", "Mane", "James", "Benard"];
+let numArr = [1, 2, 3, 4, 5];
 
 //map method
-let res = nameArr.map((name, index)=> index + name)
+let res = nameArr.map((name, index) => index + name);
 console.log(res);
 
 //filter method
-let namesWithA = nameArr.filter((name) => name.includes("a"))
-console.log(namesWithA)
+let namesWithA = nameArr.filter((name) => name.includes("a"));
+console.log(namesWithA);
+
+//reduce method
+let sum = numArr.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
