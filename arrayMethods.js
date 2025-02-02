2d7e057853bdb527e1121070b4bf049682625e3a
MandemGibson <themaingib@gmail.com>
2025-02-02 23:45:11 +0000

    add filter method to arrayMethods.js to find names containing 'a'

--- arrayMethods.js
+++ arrayMethods.js
@@ -3,3 +3,7 @@ let nameArr = ["John", "Mane","James", "Benard"]
 //map method
 let res = nameArr.map((name, index)=> index + name)
 console.log(res);
+
+//filter method
+let namesWithA = nameArr.filter((name) => name.includes("a"))
+console.log(namesWithA)
