77b51c5d0c78f65c52501178ba183c672d9ba868
MandemGibson <themaingib@gmail.com>
2025-02-11 23:18:17 +0000

    sorting function: bubble sort

---
+++ sort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 3, 8, 4, 2, 0, 12, 1]));
