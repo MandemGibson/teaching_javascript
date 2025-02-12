87f413545da72667121542899e1a0e6c5e613ab5
MandemGibson <themaingib@gmail.com>
2025-02-12 23:50:35 +0000

    add callPerson function to invoke person with sample data

--- function.js
+++ function.js
@@ -8,3 +8,8 @@ console.log(sum)
 function person({name, age}){
     console.log(`My name is ${name} and I am ${age}`);
 }
+
+//a function that calls another function
+function callPerson(){
+    person({name: 'John', age: 30})
+}
