84bb938f9acad201d0b1cc013bdc7c5d36e5c192
MandemGibson <themaingib@gmail.com>
2025-02-15 23:23:37 +0000

    deposit function

--- class.js
+++ class.js
@@ -3,4 +3,10 @@ class BankAccount {
     this.balance = 0;
     this.amount = amount
   }
+
+  deposit(amount){
+    this.balance += amount
+    console.log("An amount of ${amount} has been deposited into your account");
+    
+  }
 }
