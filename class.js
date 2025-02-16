ace3b37b5fea0e26c986d1960913e78d3499bac2
MandemGibson <themaingib@gmail.com>
2025-02-16 23:18:30 +0000

    withdraw function

--- class.js
+++ class.js
@@ -1,12 +1,15 @@
 class BankAccount {
-  constructor(amount) {
+  constructor() {
     this.balance = 0;
-    this.amount = amount
   }
 
-  deposit(amount){
-    this.balance += amount
-    console.log("An amount of ${amount} has been deposited into your account");
-    
+  deposit(amount) {
+    this.balance += amount;
+    console.log(`An amount of ${amount} has been deposited into your account`);
+  }
+
+  withdraw(amount) {
+    this.balance -= amount;
+    console.log(`An amount of ${amount} has been debited from your account`);
   }
 }
