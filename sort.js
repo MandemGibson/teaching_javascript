aa5adbb325d5e478204ff9e308a16c1e3ac5fb17
MandemGibson <themaingib@gmail.com>
2025-02-24 16:54:34 +0000

    add selection sort and insertion sort algorithms

--- sort.js
+++ sort.js
@@ -13,22 +13,37 @@ function bubbleSort(arr) {
 
 console.log(bubbleSort([5, 3, 8, 4, 2, 0, 12, 1]));
 
-function selectionSort(arr){
-  for(let i = 0; i < arr.length; i++){
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
     let min = i;
-    for(let j = i+1; j < arr.length; j++){
-      if(arr[j] < arr[min]){
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
         min = j;
       }
     }
-    if(min !== i){
+    if (min !== i) {
       let temp = arr[i];
       arr[i] = arr[min];
       arr[min] = temp;
     }
   }
 
-  return arr
+  return arr;
 }
 
 console.log(selectionSort([5, 3, 8, 4, 2, 0, 12, 1]));
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr
+}
+
+console.log(insertionSort([5, 3, 8, 4, 2, 0, 12, 1]));
