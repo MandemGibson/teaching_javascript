0de8d0dc5826d20b3aa627f6ded9fadb88a0be17
MandemGibson <themaingib@gmail.com>
2025-03-10 17:08:16 +0000

    enhance bubble sort with runtime logging and comparison count

--- sort.js
+++ sort.js
@@ -1,15 +1,28 @@
 function bubbleSort(arr) {
+  let count = 0;
+
+  console.time("Bubble Sort Runtime");
+
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
+    let swapped = false;
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+
+        swapped = true;
       }
+      count++;
     }
+    if (!swapped) break;
   }
-  console.log(arr);
+
+  console.timeEnd("Bubble Sort Runtime");
+  console.log("Sorted Array:", arr);
+  console.log("Total comparisons/swaps attempted:", count);
 }
+
 
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -27,6 +40,8 @@ function selectionSort(arr) {
   }
 
   console.log(arr);
+  console.time("Selection Sort Runtime");
+  console.timeEnd("Selection Sort Runtime");
 }
 
 function insertionSort(arr) {
@@ -40,6 +55,8 @@ function insertionSort(arr) {
     arr[j + 1] = current;
   }
   console.log(arr);
+  console.time("Insertion Sort Runtime");
+  console.timeEnd("Insertion Sort Runtime");
 }
 
 selectionSort([5, 3, 8, 4, 2, 0, 12, 1]);
